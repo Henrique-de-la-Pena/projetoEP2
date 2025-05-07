0f476a5668add8f19374c2654ce6b4d1f715c8b0
rafaelantao <rafaelba10@al.insper.edu.br>
2025-05-06 21:36:49 -0300

    Questão 10

--- funcoes.py
+++ funcoes.py
@@ -76,3 +76,15 @@ def calcula_pontos_quadra(dados_rolados):
             for valor, vezes in dados.items():
                 soma += valor * vezes
     return soma
+
+def calcula_pontos_quina(dados_rolados):
+    dados = {}
+    for i in range(len(dados_rolados)):
+        if dados_rolados[i] not in dados:
+            dados[dados_rolados[i]] = 1
+        else:
+            dados[dados_rolados[i]] += 1
+    for qtd in dados.values():
+        if qtd >= 5:
+            return 50
+    return 0
